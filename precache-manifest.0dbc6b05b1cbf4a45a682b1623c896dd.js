a3f1e831a3b2aee2c8c62984d8f9520e27f78b34
Shunmugam <shunmugam@mu-sigma.com>
2019-10-22 13:54:38 +0530

    Updates

---
+++ precache-manifest.0dbc6b05b1cbf4a45a682b1623c896dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afc35dfa37b78e6eb6bd606aad4ef73",
+    "url": "/painless-ui/index.html"
+  },
+  {
+    "revision": "226a6d9eb91c80ba7d7c",
+    "url": "/painless-ui/static/css/main.bccbcbf2.chunk.css"
+  },
+  {
+    "revision": "cef138601948ded82fec",
+    "url": "/painless-ui/static/js/2.48421b18.chunk.js"
+  },
+  {
+    "revision": "226a6d9eb91c80ba7d7c",
+    "url": "/painless-ui/static/js/main.7c2e7476.chunk.js"
+  },
+  {
+    "revision": "57e6eeb2aa9b9d3a2da5",
+    "url": "/painless-ui/static/js/runtime~main.b1ac821e.js"
+  }
+]);
